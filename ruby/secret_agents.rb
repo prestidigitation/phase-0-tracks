1d5dc18fddaf74b93b6fd5a46ca3ffb0e9370658
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-18 21:37:40 +0000

    Add decrypt method, clean up encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,19 +9,39 @@
 #    - Spaces remain unaffected
 #
 
-def encrypt(encrypted_string)
-    len = encrypted_string.length
+def encrypt(string)
+    len = string.length
     counter = 0
-    shifted_string = ""
+    encrypted_string = ""
     until len == counter
-        if encrypted_string[counter] == " "
-            shifted_string += " "
+        if string[counter] == " "
+            encrypted_string += " "
+        elsif string[counter] == "z"
+            encrypted_string += "a"
         else
-            shifted_string += encrypted_string[counter].next
+            encrypted_string += string[counter].next
         end
         counter += 1
     end
-    shifted_string
+    encrypted_string
 end
 
 puts encrypt(gets.chomp)
+
+def decrypt(encrypted_string)
+    len = encrypted_string.length
+    counter = 0
+    decrypted_string = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    until len == counter
+        if encrypted_string[counter] == " "
+            decrypted_string += " "
+        else
+            decrypted_string += alphabet[alphabet.index(encrypted_string[counter]) - 1]
+        end
+        counter += 1
+    end
+    decrypted_string
+end
+
+puts decrypt(gets.chomp)
