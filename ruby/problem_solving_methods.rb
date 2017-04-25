d4571132f59b2bd85499dff72b578d033c8ea10a
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-25 15:06:54 -0700

    Complete fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,21 +12,34 @@ def search_array(arr, num)
 end
 
 test_array = [42, 89, 23, 1]
+puts search_array(test_array, 23)
 puts search_array(test_array, 2)
 
 
-# def fib(num)
-#     fib_sequence = [0, 1]
-#     counter = 0
-#     old_num = 0
-#     current_num = 1
-#     while counter <= num
-#     counter += 1
-#     if current_num == 1 && old_num == 0
-#        old_num = 1
-#     else
-#        old_num = current_num
-#        current_num += old_num
-#        fib_sequence << current_num
-#     end
-# end
+def fib(num)
+    fib_sequence = []
+    counter = 0
+    old_num = 0
+    current_num = 1
+    while counter < num
+        counter += 1
+        if current_num <= 2
+            fib_sequence << old_num
+            placeholder = current_num
+            current_num += old_num
+            old_num = placeholder
+            puts "IF STATEMENT old_num: #{old_num}"
+            puts "IF STATEMENT current_num: #{current_num}"
+        else
+            fib_sequence << old_num
+            placeholder = current_num
+            current_num += old_num
+            old_num = placeholder
+            puts "ELSE STATEMENT old_num: #{old_num}"
+            puts "ELSE STATEMENT current_num: #{current_num}"
+        end
+    end
+    fib_sequence
+end
+
+puts fib(100)
