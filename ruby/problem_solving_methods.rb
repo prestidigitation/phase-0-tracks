ee2db054787469894640b11c14f7a4fea43774df
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-25 14:20:17 -0700

    Correct search_array method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -2,10 +2,13 @@ def search_array(arr, num)
     counter = 0
     
     arr.length.times do |value|
-        if value == num
+        if arr[value] == num
             return counter
         else
             counter += 1
         end
     end
 end
+
+test_array = [42, 89, 23, 1]
+puts search_array(test_array, 1)
