773bfbe909ce6d6a4c21827e772e0c24c3b693e6
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-25 16:32:49 -0700

    Add bubble_sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,8 +12,8 @@ def search_array(arr, num)
 end
 
 test_array = [42, 89, 23, 1]
-puts search_array(test_array, 23)
-puts search_array(test_array, 2)
+# puts search_array(test_array, 23)
+# puts search_array(test_array, 2)
 
 
 def fib(num)
@@ -28,18 +28,50 @@ def fib(num)
             placeholder = current_num
             current_num += old_num
             old_num = placeholder
-            puts "IF STATEMENT old_num: #{old_num}"
-            puts "IF STATEMENT current_num: #{current_num}"
+            # puts "IF STATEMENT old_num: #{old_num}"
+            # puts "IF STATEMENT current_num: #{current_num}"
         else
             fib_sequence << old_num
             placeholder = current_num
             current_num += old_num
             old_num = placeholder
-            puts "ELSE STATEMENT old_num: #{old_num}"
-            puts "ELSE STATEMENT current_num: #{current_num}"
+            # puts "ELSE STATEMENT old_num: #{old_num}"
+            # puts "ELSE STATEMENT current_num: #{current_num}"
         end
     end
     fib_sequence
 end
 
-puts fib(100)
+# puts fib(100)
+
+# bubble sort pseudo-code
+# start a loop that goes thru .length times one by one
+#   loop thru all others in array (.length - index)
+#       if current value is smaller than found value
+#           swap values
+#   
+def bubble_sort(arr)
+    i = 0
+    while i < arr.length
+        # puts i
+        # i += 1
+        swapped = false
+        counter = arr.length - 1
+        while counter >= i + 1
+            if arr[counter] < arr[counter - 1]
+                placeholder = arr[counter]
+                arr[counter] = arr[counter - 1]
+                arr[counter - 1] = placeholder
+                swapped = true
+            end
+            counter -= 1
+        end
+        break if !swapped
+        i += 1
+        puts arr
+    end
+    return arr
+end
+
+bubble_array = [8, 5, 3, 11, 25, 12, 14, 7]
+puts bubble_sort(bubble_array)
