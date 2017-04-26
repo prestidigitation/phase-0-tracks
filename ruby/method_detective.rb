ebf42e942fdd7f3408f07873fc3784a1cca46bf2
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-26 09:01:57 -0700

    Finish method_detective.rb, add puts in front of methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,50 +4,56 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".insert(1, "o")
+puts "zom".insert(1, "o")
 # => “zoom”
 
-"enhance".center(15)
-"enhance".ljust(11).rjust(15)
-"enhance".insert(0, "    ").insert(11, "    ")
+puts "enhance".center(15)
+puts "enhance".ljust(11).rjust(15)
+puts "enhance".insert(0, "    ").insert(11, "    ")
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".insert(9, " suspects")
-"the usual" << " suspects"
-"the usual".concat(" suspects")
-"the usual".ljust(18, " suspects")
+puts "the usual".insert(9, " suspects")
+puts "the usual" << " suspects"
+puts "the usual".concat(" suspects")
+puts "the usual".ljust(18, " suspects")
 #=> "the usual suspects"
 
-" suspects".insert(0, "the usual")
-" suspects".rjust(18, "the usual")
-" suspects".prepend("the usual")
+puts " suspects".insert(0, "the usual")
+puts " suspects".rjust(18, "the usual")
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
-"The case of the disappearing last letter".chomp("r")
-"The case of the disappearing last letter".slice(0, 39)
-"The case of the disappearing last letter"[0, 39]
+puts "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chomp("r")
+puts "The case of the disappearing last letter".slice(0, 39)
+puts "The case of the disappearing last letter"[0, 39]
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".slice(1, 38)
-"The mystery of the missing first letter"[1, 38]
-"The mystery of the missing first letter"[0] = ""
+puts "The mystery of the missing first letter".slice(1, 38)
+puts "The mystery of the missing first letter"[1, 38]
+puts "The mystery of the missing first letter"[0] = ""
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".gsub(/\s+/, " ").strip
-"Elementary,    my   dear        Watson!".split(" ").join(" ")
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Elementary,    my   dear        Watson!".gsub(/\s+/, " ").strip
+puts "Elementary,    my   dear        Watson!".split(" ").join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord
+puts "z".replace("122").to_i
+puts "z".sum
+"z".each_byte do |c|
+    puts c
+end
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
 
