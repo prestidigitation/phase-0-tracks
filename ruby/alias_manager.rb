b352df5846058f2ef286385189b8edbaa59bdd0f
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-04-27 13:54:03 -0700

    Write pseudocode for alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# 1. Swap the first and last name.
+#    - Split name string.
+#    - Recombine name string backwards with " " spacer character.
+# 2. Change all vowels to the next vowel, and all consonants to the next consonant
+#    in the alphabet.
+#    - Split name string into characters.
+#    - Iterate through characters and check to see if vowel.
+#    - If vowel, switch to next vowel.
+#    - If not vowel, switch to next consonant.
+#
