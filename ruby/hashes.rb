8e8af38cd07aadbc5c58db020fff1a6ba223d736
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-01 15:12:08 -0700

    Finish pseudocode and fix family member update prompt

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,12 +1,16 @@
-## Capitalize language-specific keywords (IF, ELSE, BREAK, etc.)
-## Indent pseudocode as you would your actual code (code blocks, conditionals, etc.)
-
 # Pseudocode
 # Initialize client_info hash.
 # Prompt user to input name.
 # Use gets.chomp to capture input name.
 # Prompt user to input age.
 # Use gets.chomp to capture input age, converting string to integer.
+# Prompt user to input number of children.
+# Use gets.chomp to capture input number, converting string to integer.
+# Initialize empty family member name array.
+# Use LOOP DO to prompt user for multiple family member name inputs.
+#   Prompt user to input family member names.
+#   IF input is equal to "done" BREAK out of loop.
+#   Append input string to family_member_list array in client_info hash.
 # Prompt user to input email.
 # Use gets.chomp to capture input email.
 # Prompt user to input phone number.
@@ -15,26 +19,36 @@
 # User gets.chomp to capture input marriage status.
 # Use UNTIL loop to nest conditional logic to capture y/n answer.
 #   Use IF/ELSE conditional logic to convert y/n answer string to boolean.
-# Prompt user to input number of children.
-# Use gets.chomp to capture input number, converting string to integer.
-# Loop through client_info hash using .each method.
+# Loop through client_info hash using .EACH DO method.
 #   Print out each key, value pair in client_info hash.
 # Initialize input_key variable.
-# Initialize input_value variable
+# Initialize input_value variable.
 # Use LOOP DO to nest code for prompting user to update client_info values.
 #   Prompt user to input hash value to update or enter none.
 #   Update input_key with gets.chomp.
 #   Use IF to BREAK loop if input_key equals "none".
 #   Convert input_key to a hash symbol.
-#   Prompt user to input new value for chosen hash.
+#   IF input_key does not equal :family_member_list
+#     Prompt user to input new value for chosen hash.
 #   Use gets.chomp to capture new input value and store in input_value.
 #   Use IF to check if input_key is "age" or "number_of_children".
 #     Convert input_value to integer and update input_key with new value.
+#   ELSIF input_key is equal to :family_member_list
+#     Loop through :family_member_list array using .EACH DO method.
+#       Prompt user to remove current family member name.
+#       IF input is equal to "y", remove name from array.
+#     LOOP DO to prompt user for adding addition family member names.
+#       Initialize empty input string variable.
+#       IF input is equal to "n", then BREAK.
+#       Append user input string to family_member_list array. 
 #   ELSIF input_key is equal to :married.
-#     Use IF block to check if input_value is equal to "y"
-#       Convert to 
-# 
-# Loop through client_info hash using .each method.
+#     Use IF block to check if input_value is equal to "y".
+#       Set value of :married to true.
+#     ELSIF input_value is equal to "n".
+#       Set value of :married to false.
+#     ELSE set input_value to nil.
+#   ELSE set client_info input_key to input_value string.
+# Loop through client_info hash using .EACH DO method.
 #   Print out each key, value pair in client_info hash.
 
 
@@ -95,9 +109,9 @@ loop do
     client_info[:family_member_list].each do |item|
       input = ""
       puts "Would you like to remove #{item} from the list (y/n)?"
-      remove = gets.chomp
-      if remove == "y"
-        client_info[:family_member_list].delete(item)
+      input = gets.chomp
+      if input == "y"
+        client_info[:family_member_list] -= [item]
       end
     end
     loop do
@@ -106,9 +120,12 @@ loop do
       input = gets.chomp
       if input == "n"
         break
+      elsif input == "y"
+        puts "Enter a name:"
+        client_info[:family_member_list] << gets.chomp
+      else
+        next
       end
-      puts "Enter a name:"
-      client_info[:family_member_list] << gets.chomp
     end
   elsif input_key == :married
     if input_value == "y"
