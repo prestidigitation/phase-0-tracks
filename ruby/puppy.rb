d372625298737d2483f91747a272c0b994f7b0b3
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-02 13:41:05 -0700

    Update puppy.rb with dog content

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------------
+# Canis lupus familiaris
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Size: varies
+
+# BEHAVIOR -----------------------
+# Fetch
+# Bark
+# Paw
+# Beg
+# Bite
+# Roll over
