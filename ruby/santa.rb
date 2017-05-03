17aa666497e4bbca53ecd392e24b262254314374
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-03 12:43:04 -0700

    Define Santa class for release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,19 @@
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
+  end
+
+end
+
+test_santa = Santa.new
+test_santa.speak
+test_santa.eat_milk_and_cookies("oreo")
