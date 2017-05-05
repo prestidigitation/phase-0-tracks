ebfa3ac5972d95e8f6b9979f196bed416d8808f9
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-04 20:40:19 -0700

    Add gender identity and ethnicity arrays, instantiate some santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-  def initialize
+  attr_reader :gender, :ethnicity, :reindeer_ranking, :age
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -14,6 +19,71 @@ class Santa
 
 end
 
-test_santa = Santa.new
-test_santa.speak
-test_santa.eat_milk_and_cookies("oreo")
+# test_santa = Santa.new("male", "white")
+# test_santa.speak
+# test_santa.eat_milk_and_cookies("oreo")
+
+# santa_array = []
+# santa_array << Santa.new("agender", "black")
+# p santa_array[0].ethnicity
+# santa_array << Santa.new("female", "Latino")
+# p santa_array[1].ethnicity
+# santa_array << Santa.new("bigender", "white")
+# p santa_array[2].ethnicity
+# santa_array << Santa.new("male", "Japanese")
+# p santa_array[3].ethnicity
+# santa_array << Santa.new("female", "prefer not to say")
+# p santa_array[4].ethnicity
+# santa_array << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# p santa_array[5].ethnicity
+# santa_array << Santa.new("N/A", "N/A")
+# p santa_array[6].ethnicity
+
+### PSEUDOCODE
+# Initialize empty array to store Santa class instances in
+# Initialize gender array with gender identities stored as strings
+# Initialize ethnicity array with ethnicities stored as strings
+# Loop X number of times
+#   Add random item from both gender_array and ethnicity_array to santa_instance_array
+
+santa_array = []
+gender_array = [
+  "Agender / genderless",
+  "Androgyne Bigender",
+  "Genderqueer / Non-binary",
+  "Gender bender",
+  "Hijra",
+  "Pangender",
+  "Queer heterosexuality",
+  "Third gender",
+  "Trans man",
+  "Trans woman",
+  "Transmasculine",
+  "Transfeminine",
+  "Trigender",
+  "Two-Spirit"]
+ethnicity_array = [
+  "Albanian",
+  "Afrikaner",
+  "Berber",
+  "Zulu",
+  "Czech",
+  "Japanese",
+  "Georgians",
+  "Icelander",
+  "Hindustani",
+  "Javanese",
+  "Malayali",
+  "Mongol",
+  "Parsi",
+  "Romani",
+  "Russian",
+  "Shona",
+  "Tibetan",
+  "Uyghur"
+]
+
+20.times do 
+  santa_array << Santa.new(gender_array.sample, ethnicity_array.sample)
+end
+p santa_array
