13ce4fd88ff2ed2018c74f3f784a2d5995c82a37
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-07 22:48:58 -0700

    Write driver code

--- ruby/guessing_game/guessing_game.rb
+++ ruby/guessing_game/guessing_game.rb
@@ -1,27 +1,30 @@
-# Pseudocode
+### PSEUDOCODE ###
 # 
 # Prompt user_one to input a word or phrase to guess
 # Initialize guessed_correctly variable to keep track of overall game state
 # Initialize revealed_letters array of "-" characters equal to length of input word
 # Initialize maximum_guesses variable equal to the length of input word
-# WHILE guess_correctly is false
-#   Prompt user_two for letter to input
+# Initialize guess_number variable to store number of guesses user_two has made
+# WHILE guess_correctly is false and guess_number is less than maximum_guesses 
+#   Prompt user for letter to input or to guess the entire word
 #   Iterate through revealed_letters array, replacing dashes with respective input letter (requires comparing revealed_letters to the original input word)
 #   Inform user if their letter wasn't found
-#   
+# Tell user they have won if guess_correctly is true
+# Tell user they have lost if guess_number >= maximum_guesses
 
 class GuessingGame
-  attr_accessor :revealed_letters
-  attr_reader :guessed_correctly
+  attr_reader :guessed_correctly, :guess_number, :maximum_guesses, :revealed_letters
 
   def initialize(target_word)
     @guessed_correctly = false
     @target_word = target_word
+    @guess_number = 0
     @maximum_guesses = @target_word.length / 2
     @revealed_letters = Array.new(@target_word.length) { "-" }
   end
 
   def search_for_letter(letter)
+    @guess_number += 1
     letter_found = false
     @target_word.length.times do |i|
       if @target_word[i] == letter
@@ -37,10 +40,38 @@ class GuessingGame
   end
 
   def guess_word(word)
+    @guess_number += 1
     if word == @target_word
       @guessed_correctly = true
     end
     @guessed_correctly
   end
 
+end
+
+### DRIVER CODE ###
+puts "Player \#1, please enter a word for player \#2 to guess:"
+input = gets.chomp.downcase
+game = GuessingGame.new(input)
+while !game.guessed_correctly && game.guess_number <= game.maximum_guesses
+  p game.revealed_letters
+  puts "Would you like to guess a letter or the entire word (l/w)?"
+  loop do
+    input = gets.chomp.downcase
+    if input == "l"
+      puts "Enter a letter to guess:"
+      game.search_for_letter(gets.chomp.downcase)
+      break
+    elsif input == "w"
+      puts "Enter a word to guess:"
+      game.guess_word(gets.chomp.downcase)
+      break
+    end
+  end
+end
+
+if game.guessed_correctly
+  puts "Congratulations! You won the game!"
+else
+  puts "You lost! You are a terrible player!"
 end
--- ruby/guessing_game/guessing_game_spec.rb
+++ ruby/guessing_game/guessing_game_spec.rb
@@ -6,17 +6,19 @@ describe GuessingGame do
   end
 
   it "Should return array of revealed letters" do
-    expect(@test_game.revealed_letters).to eq(["-", "-", "-", "-", "-", "-", "-"])
+    expect(@test_game.revealed_letters).to eq(["-", "-", "-", "-", "-", "-", "-", "-"])
   end
 
   it "Should find all occurrences of letter \"a\" in aadvark" do
-    expect(@test_game.search_for_letter("a")).to eq(["a", "a", "-", "-", "a", "-", "-"])
+    expect(@test_game.search_for_letter("a")).to eq(["a", "a", "-", "-", "-", "a", "-", "-"])
   end
 
   it "Should say that letter wasn't found" do
     expect(@test_game.search_for_letter("b")).to eq("I'm sorry, but your letter wasn't found")
   end
 
+
+# guess_word tests, method returns true or false
   it "Should say that guessed word is true" do
     expect(@test_game.guess_word("aadvark")).to eq(true)
   end
