327c4bacd2dce2c60fd05c1b5806423a2bf58470
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-10 00:50:24 -0700

    Write code to reverse a string

--- ruby/explore.js
+++ ruby/explore.js
@@ -5,3 +5,13 @@
 //   Concatenate reversed_string and the current character of input string.
 // Return reversed_string.
 
+function reverse(string) {
+    var reversed_string = "";
+    for (i = string.length - 1; i >= 0; i--) {
+        reversed_string += string[i];
+    }
+    return reversed_string;
+}
+
+test_string = "buckaw";
+reversed_test_string = reverse(test_string);
