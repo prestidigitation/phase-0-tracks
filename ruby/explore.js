24b950fdecf8c0598dbd6e2dfac4ab82369c9514
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-10 00:36:30 -0700

    Write pseudocode

---
+++ ruby/explore.js
@@ -0,0 +1,7 @@
+// Define function that takes a string as an input.
+// Initialize an empty string variable, reversed_string.
+// Create a loop that iterates a number of times equal to the length of input string.
+// The iteration should count downwards, iterating through each character of input string.
+//   Concatenate reversed_string and the current character of input string.
+// Return reversed_string.
+
