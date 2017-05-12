f8b33e5917c81dcc9b1d4f6d2adf03e166926015
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-11 18:52:22 -0700

    Create car constructor with function

--- js/data_structures.js
+++ js/data_structures.js
@@ -23,3 +23,23 @@ for (i = 0; i < colorArray.length; i++) {
 }
 
 console.log(horses);
+
+
+function Car(make, model, year, nitrous) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.nitrous = nitrous;
+
+    this.honk = function() { console.log("BEEP BEEP!"); };
+}
+
+var honda = new Car("Honda", "Civic", 1991, true);
+honda.honk();
+
+console.log(honda);
+
+var mercedes = new Car("Mercedes", "S600", 2017, false);
+
+console.log(mercedes.model);
+console.log(mercedes.nitrous);
