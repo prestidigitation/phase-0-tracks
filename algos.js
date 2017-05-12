48c68f8c3478b3de668804b621c4d1991ef4caea
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-11 17:49:15 -0700

    Write pseudocode

---
+++ algos.js
@@ -0,0 +1,5 @@
+// Define function that takes an array as input
+// Initialize longest_phrase variable to store longest item from input array
+// Loop through each item in input array
+//   Compare current item to longest_phrase and update it if current item length is longer
+// Return longest_phrase variable
