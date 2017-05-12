ae4b1a25ca431c586e8916d37ba2f76af4ff4dd7
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-11 18:32:08 -0700

    Create array of colors, another of horse names

---
+++ js/data_structures.js
@@ -0,0 +1,18 @@
+var colorArray = [
+    "blue",
+    "green",
+    "red",
+    "gold"
+];
+
+var horseNameArray = [
+    "Ed",
+    "Secretariat",
+    "Sausage",
+    "Glue"
+];
+
+colorArray.push("cerulean");
+console.log(colorArray);
+horseNameArray.push("John Snow");
+console.log(horseNameArray);
