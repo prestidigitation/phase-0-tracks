df28a61517133c57d3c7f820f476e0bd9f17c5f6
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-11 17:57:40 -0700

    Write code for longestPhrase function

--- algos.js
+++ algos.js
@@ -3,3 +3,19 @@
 // Loop through each item in input array
 //   Compare current item to longest_phrase and update it if current item length is longer
 // Return longest_phrase variable
+
+function longestPhrase(arrayOfStrings) {
+    longest_phrase = "";
+    for (i = 0; i < arrayOfStrings.length; i++) {
+        if (arrayOfStrings[i].length > longest_phrase.length) {
+            longest_phrase = arrayOfStrings[i];
+        }
+    }
+    return longest_phrase;
+}
+
+testArrayOne = ["hello", "my", "baby", "crocodile", "mother"];
+console.log(longestPhrase(testArrayOne));
+
+testArrayTwo = ["onesies", "are", "in", "fashion"];
+console.log(longestPhrase(testArrayTwo));
