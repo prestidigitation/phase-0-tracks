7860ba58248b6cf3b1d7a468a6196634b96ee80f
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-24 11:07:06 -0700

    Make reveal button target chameleon image

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -9,12 +9,22 @@ function addPinkBorder(event) {
 function hideLizard(event) {
   console.log("click happened! here's the click event:");
   console.log(event);
-  // photo.hidden = true;
-  photo.style.display = "none";
+  photo.hidden = true;
+  // photo.style.display = "none";
+}
+
+function revealLizard(event) {
+  console.log("click happened! here's the click event:");
+  console.log(event);
+  photo.hidden = false;
+  // photo.style.display = "none";
 }
 
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", addPinkBorder);
 
-var button = document.getElementById("hide_button");
-button.addEventListener("click", hideLizard);
+var button_one = document.getElementById("hide_button");
+button_one.addEventListener("click", hideLizard);
+
+var button_two = document.getElementById("reveal_button");
+button_two.addEventListener("click", revealLizard);
