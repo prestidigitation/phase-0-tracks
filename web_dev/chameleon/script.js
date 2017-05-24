12a00b19f6a2a3f6a45beaad2c13177cddae9ec4
Alexander Rowland <alexander.c.rowland@gmail.com>
2017-05-24 11:03:49 -0700

    Make hiding button target chameleon image

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -6,5 +6,15 @@ function addPinkBorder(event) {
   event.target.style.border = "2px solid pink";
 }
 
+function hideLizard(event) {
+  console.log("click happened! here's the click event:");
+  console.log(event);
+  // photo.hidden = true;
+  photo.style.display = "none";
+}
+
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", addPinkBorder);
+
+var button = document.getElementById("hide_button");
+button.addEventListener("click", hideLizard);
